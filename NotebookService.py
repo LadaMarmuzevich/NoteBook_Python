053481ab1a4a3d2960bf6f8b4a1d97caedb65c7e
Lada Marmuzevich <ladabeet871@gmail.com>
2024-07-17 12:07:17 +0300

    Добавлена функция main

--- NotebookService.py
+++ NotebookService.py
@@ -97,3 +97,23 @@ def delete_note():
     notes = [note for note in notes if note["id"] != note_id]
     save_notes(notes)
     print("Заметка успешно удалена.")
+
+
+def main():
+    while True:
+        command = input("Введите команду (add, read, read_date, edit, delete, exit): ")
+
+        if command == "add":
+            add_note()
+        elif command == "read":
+            read_notes()
+        elif command == "read_date":
+            read_notes_by_date()
+        elif command == "edit":
+            edit_note()
+        elif command == "delete":
+            delete_note()
+        elif command == "exit":
+            break
+        else:
+            print("Неверная команда.")
